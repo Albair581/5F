52212eb8e2287270804d26a0ecdb3fd3a21e7e3e
Albert Huang <albertoyucheng@gmail.com>
2025-06-02 18:41:02 +0800

    new book

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function loadLanguage(lang) {
 
 // NOTE:
 // ---------------------------------------------------------------------------------//
-const opened_books = ["000", "001", "003", "004"];
+const opened_books = ["000", "001", "003", "004", "005"];
 const authorized = [
     "staff-albert", "staff-ray", "staff-ethan", "staff-marcus", "staff-sophia", // staff
     "cont-wilbur", "cont-champ", "cont-ian", "cont-ivan", // contributors
